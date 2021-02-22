0f74fa8218555a9a25a69ea75e2ab860e4d2b379
Andrey Shklyaruk <13mailg@gmail.com>
2021-02-22 18:51:51 +0200

    progression info fix

--- src/components/ProgressionIndicator.js
+++ src/components/ProgressionIndicator.js
@@ -8,15 +8,17 @@ export default function ProgressionIndicator() {
   const notesCount = notes.length
   const completedNotesCount = notes.filter((note) => note.completed).length
   const currentProgress = (100 / notesCount) * completedNotesCount
+  const infoText =
+    notesCount === completedNotesCount
+      ? 'You have completed all notes'
+      : `You have ${completedNotesCount}/${notesCount} notes completed`
 
   return (
     <div
       className={classes.root}
       style={{ display: notesCount ? 'block' : 'none' }}
     >
-      <div className={classes.infoText}>
-        {`You have ${completedNotesCount}/${notesCount} notes completed`}
-      </div>
+      <div className={classes.infoText}>{infoText}</div>
       <LinearProgress variant="determinate" value={currentProgress} />
     </div>
   )
